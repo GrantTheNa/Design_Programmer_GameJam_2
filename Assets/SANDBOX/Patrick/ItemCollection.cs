d206d85147561127e2af28303b3f9413ad4ee6b3
Patrick <patrick.astermann01@gmail.com>
2021-09-20 13:39:55 +1000

    Item Collection and Player Stats
    Scripts for item collection and changing player stats.

---
+++ Assets/SANDBOX/Patrick/ItemCollection.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ItemCollection : MonoBehaviour
+{
+    //VARS
+    public float dmgUp = 10;
+    public float spdUp = 10;
+
+    //REFS
+    public PlayerStats pS;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            pS.plyrDmg += dmgUp;
+            pS.UpdateText();
+            Debug.Log("I have eaten");
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Drink"))
+        {
+            pS.plyrSpd += spdUp;
+            pS.UpdateText();
+            Debug.Log("I have drunk");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/SANDBOX/Patrick/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    //VARS
+    public Text dmgText;
+    public Text spdText;
+    public float plyrDmg = 100;
+    public float plyrSpd = 100;
+    public float plyrHlth = 100;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dmgText.text = plyrDmg.ToString();
+        spdText.text = plyrSpd.ToString();
+    }
+
+    public void UpdateText()
+    {
+        dmgText.text = plyrDmg.ToString();
+        spdText.text = plyrSpd.ToString();
+    }
+}
