246bab45efe3695000587c619ec90b60329b2339
James Webster <s200676@students.aie.edu.au>
2021-09-22 15:23:37 +1000

    Adjusted Building scale for destroy worlds (D_W 2 has been deleted and must be recovered.

--- Assets/Scripts/02_PLAYER/PlayerController.cs
+++ Assets/Scripts/02_PLAYER/PlayerController.cs
@@ -33,8 +33,14 @@ public class PlayerController : MonoBehaviour
             Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
 
+            Debug.Log(myRay);
+            
+
+            Debug.Log("mouse was clicked");
+
             if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
             {
+                Debug.Log("Click hit the ground");
                 myAgent.SetDestination(hitInfo.point);
                 destination = hitInfo.point;
             }
