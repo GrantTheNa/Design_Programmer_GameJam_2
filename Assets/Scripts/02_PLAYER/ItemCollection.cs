908fd0b784553287762ca7c7c21d8fcba5d68859
Omar Samad <omar.samad6@gmail.com>
2021-09-22 12:06:56 +1000

    player ui and prefab

---
+++ Assets/Scripts/02_PLAYER/ItemCollection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ItemCollection : MonoBehaviour
+{
+    //VARS
+    public float dmgUp = 10;
+    public float spdUp = 10;
+
+    //REFS
+    public PlayerStats pS;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            pS.plyrGrth += 1;
+            pS.plyrDmg += dmgUp;
+            pS.GrowCheck();
+            pS.UpdateDmg();
+            Debug.Log("I have eaten");
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Drink"))
+        {
+            pS.plyrGrth += 1;
+            pS.plyrSpd += spdUp;
+            pS.GrowCheck();
+            pS.UpdateSpd();
+            Debug.Log("I have drunk");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/02_PLAYER/PlayerStats.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerStats : MonoBehaviour
+{
+    //VARS
+    public Text dmgText;
+    public Text spdText;
+    public float plyrDmg = 10;
+    public float plyrSpd = 10;
+    public float plyrHlth = 100;
+    public float plyrAge = 10;
+    public int plyrGrth = 0;
+    public Sprite teenAlien;
+    public Sprite preAdAlien;
+    public Sprite adultAlien;
+    public GameObject aSprite;
+
+    [Header("UI GOs")]
+    [SerializeField] private GameObject healthUI;
+    [SerializeField] private GameObject speedUI;
+    [SerializeField] private GameObject attackUI;
+
+    [Header("UI Sliders")]
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Slider speedSlider;
+    [SerializeField] private Slider attackSlider;
+
+
+    //REFS
+    public PlayerController pC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //dmgText.text = plyrDmg.ToString();
+        //spdText.text = plyrSpd.ToString();
+
+        //NEW - UI
+        
+    }
+
+
+
+    public void GrowCheck()
+    {
+        if (plyrGrth == 5)
+        {
+            aSprite.GetComponent<SpriteRenderer>().sprite = teenAlien;
+            plyrDmg += plyrAge;
+            plyrSpd += plyrAge;
+            UpdateSpd();
+            UpdateDmg();
+            Debug.Log("I have aged");
+        }
+        else if (plyrGrth == 10)
+        {
+            aSprite.GetComponent<SpriteRenderer>().sprite = preAdAlien;
+            plyrDmg += plyrAge;
+            plyrSpd += plyrAge;
+            UpdateSpd();
+            UpdateDmg();
+            Debug.Log("I have aged");
+        }
+        else if (plyrGrth == 15)
+        {
+            aSprite.GetComponent<SpriteRenderer>().sprite = adultAlien;
+            plyrDmg += plyrAge;
+            plyrSpd += plyrAge;
+            UpdateSpd();
+            UpdateDmg();
+            Debug.Log("I have aged");
+        }
+    }
+
+    public void UpdateDmg()
+    {
+        dmgText.text = plyrDmg.ToString();
+    }
+
+    public void UpdateSpd()
+    {
+        pC.playerSpeed = plyrSpd;
+        spdText.text = plyrSpd.ToString();
+    }
+
+    public void SetPowerUpLevelUI()
+    {
+        healthUI.SetActive(false);
+        speedUI.SetActive(true);
+        attackUI.SetActive(true);
+    }
+
+    public void SetDestroyWorldUI()
+    {
+        healthUI.SetActive(true);
+        speedUI.SetActive(false);
+        attackUI.SetActive(false);
+    }
+
+}
